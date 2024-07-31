15ba397e5554a0d09386c41d162152792e8a81a8
dileepbhupathi <98819935+dileepbhupathi@users.noreply.github.com>
2024-07-31 15:35:34 +0530

    changes be

---
+++ todo-be/api/server.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Todo = require('../models/todo-model');
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: process.env.CORS_ALLOWED_ORIGIN || '*',
+}));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('DB connected...'))
+  .catch((error) => console.log('DB connection error:', error));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/addTodo', async (req, res) => {
+  const { todo } = req.body;
+  try {
+    const newTodo = new Todo({ todo });
+    await newTodo.save();
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    console.error('Error adding todo:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.get('/todoList', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    console.error('Error fetching todos:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.put('/editTodo/:id', async (req, res) => {
+  try {
+    await Todo.findByIdAndUpdate(req.params.id, req.body);
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    console.error('Error editing todo:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.delete('/deleteTodo/:id', async (req, res) => {
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    console.error('Error deleting todo:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
