be94c4d792ea2ec924f4ce0a246ebc601af94669
dileepbhupathi <98819935+dileepbhupathi@users.noreply.github.com>
2024-07-31 15:43:06 +0530

    mongoose changes

--- todo-be/api/server.js
+++ todo-be/api/server.js
@@ -1,26 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const Todo = require('../models/todo-model');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Todo = require("../models/todo-model");
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: process.env.CORS_ALLOWED_ORIGIN || '*',
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ALLOWED_ORIGIN || "*",
+  }),
+);
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('DB connected...'))
-  .catch((error) => console.log('DB connection error:', error));
+mongoose
+  .connect(
+    "mongodb+srv://dileepbhupathi97:Dileep97@cluster0.znmqgls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  )
+  .then(() => console.log("DB connected..."))
+  .catch((error) => console.log("DB connection error:", error));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.post('/addTodo', async (req, res) => {
+app.post("/addTodo", async (req, res) => {
   const { todo } = req.body;
   try {
     const newTodo = new Todo({ todo });
@@ -28,40 +35,40 @@ app.post('/addTodo', async (req, res) => {
     const todos = await Todo.find();
     res.json(todos);
   } catch (error) {
-    console.error('Error adding todo:', error);
-    res.status(500).send('Server error');
+    console.error("Error adding todo:", error);
+    res.status(500).send("Server error");
   }
 });
 
-app.get('/todoList', async (req, res) => {
+app.get("/todoList", async (req, res) => {
   try {
     const todos = await Todo.find();
     res.json(todos);
   } catch (error) {
-    console.error('Error fetching todos:', error);
-    res.status(500).send('Server error');
+    console.error("Error fetching todos:", error);
+    res.status(500).send("Server error");
   }
 });
 
-app.put('/editTodo/:id', async (req, res) => {
+app.put("/editTodo/:id", async (req, res) => {
   try {
     await Todo.findByIdAndUpdate(req.params.id, req.body);
     const todos = await Todo.find();
     res.json(todos);
   } catch (error) {
-    console.error('Error editing todo:', error);
-    res.status(500).send('Server error');
+    console.error("Error editing todo:", error);
+    res.status(500).send("Server error");
   }
 });
 
-app.delete('/deleteTodo/:id', async (req, res) => {
+app.delete("/deleteTodo/:id", async (req, res) => {
   try {
     await Todo.findByIdAndDelete(req.params.id);
     const todos = await Todo.find();
     res.json(todos);
   } catch (error) {
-    console.error('Error deleting todo:', error);
-    res.status(500).send('Server error');
+    console.error("Error deleting todo:", error);
+    res.status(500).send("Server error");
   }
 });
 
